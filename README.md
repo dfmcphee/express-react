e989d5bac08c2fcce9a657660ba9efe35b7bd05c
Dominic McPhee <dominic.mcphee@gmail.com>
2016-03-19 09:34:24 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Express React
+This is a dead simple boilerplate for a client server JavaScript app using Express and React.
+
+## Getting started
+1. `npm install`
+2. `npm start`
+
+## Building for production
+`npm run build`
---
+++ app/components/hello/hello.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './hello.css';
+
+export default class Hello extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: 'Loading...'
+    };
+
+    this.fetchMessage();
+  }
+
+  fetchMessage() {
+    fetch('/message.json')
+      .then((response) => response.json())
+      .then((data) => this.setState({ message: data.message }));
+  }
+
+  render() {
+    return (
+      <h1 className={styles.hello}>{this.state.message}</h1>
+    )
+  }
+}
